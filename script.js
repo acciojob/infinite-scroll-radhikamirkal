cc55d361fc1def9fc42d0681a2db7abc1133c9aa
Radhika Irkal <radhikamirkal@gmail.com>
2026-02-01 13:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+// Function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.innerText = `Item ${count}`;
+    list.appendChild(li);
+    count++;
+  }
+}
+
+// Add 10 items initially
+addItems(10);
+
+// Listen for scroll event
+list.addEventListener("scroll", () => {
+  // Check if user reached bottom
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
 
